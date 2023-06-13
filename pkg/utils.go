e7a649c8bff5258be26ab0540313eeddaa891cab
Francisco <frangarcia38@hotmail.com>
2023-06-12 20:35:51 -0600

    Helpers for writing and reading json

---
+++ pkg/utils.go
@@ -0,0 +1,30 @@
+package pkg
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+func WriteJSON(w http.ResponseWriter, statusCode int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func ReadJSON(r *http.Request, data any) error {
+	dec := json.NewDecoder(r.Body)
+
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must have only a single JSON value")
+	}
+
+	return nil
+}
